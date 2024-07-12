ed13b3ca9687688abcc1a2781aa6f54f3d2b303f
Trystan <trystansiew@gmail.com>
2024-07-12 14:38:52 +0800

    add css & header
    commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function Header (){
+    return (
+        <header className='header'>
+            <p>About Me</p>
+            <p>Projects</p>
+            <p>Contact</p>
+        </header>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
