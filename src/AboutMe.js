5ceaf74e576d7cba1620532a921e686af8c250d1
Trystan <trystansiew@gmail.com>
2024-07-17 22:06:05 +0800

    add skill logo and contact hyperlink

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,5 +1,13 @@
 import './AboutMe.css'
 import pic from './pic.jpg'
+import reactLogo from './reactLogo.png'
+import d3Logo from './d3Logo.png'
+import postgresqlLogo from './PostgreSQL.png'
+import jsLogo from './javascriptLogo.png'
+import htmlLogo from './htmlLogo.png'
+import phpLogo from './phpLogo.png'
+import cssLogo from './cssLogo.png'
+import vueLogo from './vueLogo.png'
 
 export default function AboutMe(){
     return (
@@ -8,13 +16,14 @@ export default function AboutMe(){
             <div id="about-section">
                 <img id="pic" src={pic} alt="pic"></img>
                 <div id="skillbox">
-                    <div class="skills">React</div>
-                    <div class="skills">D3</div>
-                    <div class="skills">PostgreSQL</div>
-                    <div class="skills">Javascripts</div>
-                    <div class="skills">PHP</div>
-                    <div class="skills">HTML</div>
-                    <div class="skills">CSS</div>
+                    <div class="skills"><img src={reactLogo} alt="reactLogo" class="skill-logo"></img>React</div>
+                    <div class="skills"><img src={vueLogo} alt="vueLogo" class="skill-logo"></img>Vue</div>
+                    <div class="skills"><img src={d3Logo} alt="d3Logo" class="skill-logo"></img>D3</div>
+                    <div class="skills" id="postgre"><img src={postgresqlLogo} id="postgreLogo" alt="postgresqlLogo" class="skill-logo"></img>PostgreSQL</div>
+                    <div class="skills" id="js"><img src={jsLogo} alt="jsLogo" class="skill-logo"></img>Javascripts</div>
+                    <div class="skills"><img src={phpLogo} alt="phpLogo" class="skill-logo"></img>PHP</div>
+                    <div class="skills"><img src={htmlLogo} alt="htmlLogo" class="skill-logo"></img>HTML</div>
+                    <div class="skills"><img src={cssLogo} alt="cssLogo" class="skill-logo"></img>CSS</div>
                 </div>
                 <p id="about-desc">2017 was the year I stepped into the tech industry. 
                     As a service desk agent, I gained exposure to various software and environments. 
@@ -34,7 +43,7 @@ export default function AboutMe(){
                     These challenges are frustrating, but they had never failed to push me to strive for improvement. 
                     It has taken a lot of dedication to reach this point, to really call myself ready to approach the path of getting a developer job.
                     If you are reading this message, thanks for coming this far. I hope you would carry on with the review of my profile. 
-                    Please consider reaching out, be it a word of encouragement, suggestions, or simply a "hi"! 
+                    Please consider <a id="reachingOut" href="#Contact">reaching out</a>, be it a word of encouragement, suggestions, or simply a "hi"! 
                 </p>
             </div>
         </div>
