0afca1c7ece8cd8d5a087cf4109abcd8644588c6
Trystan <trystansiew@gmail.com>
2024-08-01 22:04:32 +0800

    editted home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './img/logo.svg';
 import './App.css';
+import coder from './img/21004063.png'
 
 function App() {
   return (
     <div className="App" id="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          test again
-          test again 2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={coder} className="coder" alt="coder" />
+        <h3 class="typewriter">Hi! My name is Trystan,<br />a self-learned coder,<br />a full stack developer wanna-be.</h3>
       </header>
     </div>
   );
